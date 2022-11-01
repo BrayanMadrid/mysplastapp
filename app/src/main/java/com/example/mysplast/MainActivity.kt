4c1e9be218cc95135f10ae8af321ea6b462eb93b
BrayanMadrid <b.rayan192010@hotmail.com>
2022-11-01 07:16:58 -0500

    Actualización 01/11/2022

---
+++ app/src/main/java/com/example/mysplast/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.mysplast
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mysplast.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val correo = intent.extras
+        val correoobtenido = correo?.getString("correo").toString()
+
+        val nombre = intent.extras
+        val nombreobtenido = nombre?.getString("nombre").toString()
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        val header: View = navigationView.getHeaderView(0)
+
+
+        val lbNombreUsuario:TextView = header.findViewById(R.id.lbNombreUsuario)
+        val lbCorreoUsuario:TextView  = header.findViewById(R.id.lbCorreoUsuario)
+        lbNombreUsuario.setText(nombreobtenido)
+        lbCorreoUsuario.setText(correoobtenido)
+        lbNombreUsuario.setOnClickListener{
+            lbNombreUsuario.text = nombreobtenido
+        }
+        lbCorreoUsuario.setOnClickListener{
+            lbCorreoUsuario.text = correoobtenido
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
--- app/src/main/java/com/example/mysplast/activitys/LoginActivity.kt
+++ app/src/main/java/com/example/mysplast/activitys/LoginActivity.kt
@@ -1,13 +1,16 @@
 package com.example.mysplast.activitys
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.util.Base64
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mysplast.MainActivity
 import com.example.mysplast.R
 import com.example.mysplast.model.Payload
 import com.example.mysplast.model.Token
@@ -39,57 +42,74 @@ class LoginActivity : AppCompatActivity() {
     private fun iniciarSesion() {
         var edtUsuario = findViewById<EditText>(R.id.edtUsuario).text.toString()
         var edtPassword = findViewById<EditText>(R.id.edtContraseña).text.toString()
-        var call =  apiService.autenticarse("Basic YW5ndWxhcmFwcDoxMjM0NQ==",
-            edtUsuario,
-            edtPassword,
-            "password")
-        call.enqueue(object : Callback<Token>{
-            override fun onResponse(call: Call<Token>, response: Response<Token>) {
-                if(response.isSuccessful){
-                    var respuesta = response.body()
-                    if(respuesta==null){
-                        Toast.makeText(applicationContext, "Error en el Servidor", Toast.LENGTH_LONG).show()
-                        return
-                    }
-
-                    if(respuesta.accesstoken!=null){
-                        val mpref =  getSharedPreferences("token", MODE_PRIVATE)
-                        val prefusuario =  getSharedPreferences("usuario", MODE_PRIVATE)
-                        var editorusuario = prefusuario.edit()
-                        var editor = mpref.edit()
-                        editor.putString("accesstoken",respuesta.accesstoken)
-                        editor.commit()
-
-                        var token = Token(respuesta.accesstoken, respuesta.refreshToken)
-
-                        payloadtokeeeen =(respuesta.accesstoken).split("\\.".toRegex())[1]
-                        var objeto = Base64.decode(payloadtokeeeen, Base64.DEFAULT)
-                        var texto: String = String(objeto, StandardCharsets.UTF_8)
-                        var g = Gson()
-                        var  payload: Payload = g.fromJson(texto, Payload::class.java)
-
-                        editorusuario.putString("accesstoken",payload.user_name)
-                        editorusuario.commit()
-                        Log.d("Username", payload.user_name)
-                        Log.d("Correo", payload.correo)
-                    } else {
-                        if (edtUsuario.isEmpty() || edtPassword.isEmpty()
-                        ) {
-                            Log.d("ERRROOOOOORRR", "Todo es Obligatorio " )
-                        } else {
-                            Log.d("ERRROOOOOORRR", "Inicio de Ssion incorrecto ")
+        if (edtUsuario.isEmpty() || edtPassword.isEmpty()){
+            val texto = "Todos los campos son obligatorios!"
+            val duracion = Toast.LENGTH_LONG
+            val toast = Toast.makeText(applicationContext, texto, duracion)
+            toast.show()
+    } else {
+            var call = apiService.autenticarse(
+                "Basic YW5ndWxhcmFwcDoxMjM0NQ==",
+                edtUsuario,
+                edtPassword,
+                "password"
+            )
+            call.enqueue(object : Callback<Token> {
+                override fun onResponse(call: Call<Token>, response: Response<Token>) {
+                    if (response.isSuccessful) {
+                        var respuesta = response.body()
+                        if (respuesta == null) {
+                            Toast.makeText(
+                                applicationContext,
+                                "Error en el Servidor",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            return
                         }
+
+                        if (respuesta.accesstoken != null) {
+                            val mpref = getSharedPreferences("token", MODE_PRIVATE)
+                            val prefusuario = getSharedPreferences("usuario", MODE_PRIVATE)
+                            var editorusuario = prefusuario.edit()
+                            var editor = mpref.edit()
+                            editor.putString("accesstoken", respuesta.accesstoken)
+                            editor.commit()
+
+                            var token = Token(respuesta.accesstoken, respuesta.refreshToken)
+
+                            payloadtokeeeen = (respuesta.accesstoken).split("\\.".toRegex())[1]
+                            var objeto = Base64.decode(payloadtokeeeen, Base64.DEFAULT)
+                            var texto: String = String(objeto, StandardCharsets.UTF_8)
+                            var g = Gson()
+                            var payload: Payload = g.fromJson(texto, Payload::class.java)
+
+                            editorusuario.putString("accesstoken", payload.user_name)
+                            editorusuario.commit()
+                            Log.d("Username", payload.user_name)
+                            Log.d("Correo", payload.correo)
+                            val intent =  Intent(applicationContext, MainActivity::class.java).apply {
+                                putExtra("username",  payload.user_name)
+                                putExtra("correo",payload.correo)
+                                putExtra("nombre",payload.nombre)
+                            }
+                            startActivity(intent)
+                        } else {
+                                Log.d("ERRROOOOOORRR", "Inicio de Ssion incorrecto ")
+                        }
+                    } else {
+                        val texto = "Error, los datos ingresados son erroneos!"
+                        val duracion = Toast.LENGTH_LONG
+                        val toast = Toast.makeText(applicationContext, texto, duracion)
+                        toast.show()
                     }
-                } else {
-                    Log.d("TOKEEEEN", "ERRRRRRRRRRRRRRRRRROR")
                 }
-            }
 
-            override fun onFailure(call: Call<Token>, t: Throwable) {
-                Log.d("ERRROOOOOORRR", "Datos Incorrectos " + t.message)
-            }
+                override fun onFailure(call: Call<Token>, t: Throwable) {
+                    Log.d("ERRROOOOOORRR", "Datos Incorrectos " + t.message)
+                }
 
-        })
+            })
 
+        }
     }
 }
